d144802d1d758ee1c871186db4a0aaf3e017b08e
prajnadamodar <prajnadamodar98@gmail.com>
2026-08-17 12:37:06 +0530

    Change instance type from t2.micro to t3.micro

--- EC2-Creation/main.tf
+++ EC2-Creation/main.tf
@@ -4,5 +4,5 @@ provider "aws" {
 
 resource "aws_instance" "example" {
     ami           = "ami-0b6d9d3d33ba97d99"  # Specify an appropriate AMI ID
-    instance_type = "t2.micro"
+    instance_type = "t3.micro"
 }
